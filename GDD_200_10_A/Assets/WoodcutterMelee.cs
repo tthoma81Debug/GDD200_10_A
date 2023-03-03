cdf4e22c53e3d448b13176b7d2e51648bca8b973
thomastyl <tthoma81@gmail.com>
2023-03-03 10:53:12 -0600

    Fireball_Coroutine_Spawn_New_Projectile

--- GDD_200_10_A/Assets/WoodcutterMelee.cs
+++ GDD_200_10_A/Assets/WoodcutterMelee.cs
@@ -11,6 +11,16 @@ public class WoodcutterMelee : MonoBehaviour
 
     GameObject theGameManager;
     Inventory theInventoryScript;
+
+    public GameObject fireballPrefab;
+    private GameObject spawnedFireball;
+    private GameObject spawnPoint;
+
+    private IEnumerator theCoroutine;
+
+    private bool onTimer = false;
+
+
     void Start()
     {
         woodcutterAnimator = GetComponent<Animator>();
@@ -19,6 +29,7 @@ public class WoodcutterMelee : MonoBehaviour
 
         theGameManager = GameObject.Find("GameManager");
         theInventoryScript = theGameManager.GetComponent<Inventory>();
+        spawnPoint = GameObject.Find("SpawnPoint");
     }
 
     // Update is called once per frame
@@ -40,11 +51,58 @@ public class WoodcutterMelee : MonoBehaviour
             if(fireball != null)
             {
                 Debug.Log("Attacking with " + fireball.name);
+
+                //spawn fireball
+
+                if(onTimer == false)
+                {
+                    //spawnedFireball = Instantiate(fireballPrefab, spawnPoint.transform.position, Quaternion.identity);
+                    StartCoroutine(fireballDelay());
+                }
+               
+                
+
+
             }
             else
             {
                 Debug.Log("Pocket sun not in inventory. Cannot magic attack");
             }
+        }
+    }
+
+    private IEnumerator fireballDelay()
+    {
+        while (true)
+        {
+
+
+            onTimer = true;
+            spawnedFireball = Instantiate(fireballPrefab, spawnPoint.transform.position, Quaternion.identity);
+
+            //does this code on the frame it is started
+            //yield return null; //waits here until the next frame
+            //does this code after the frame has passed
+
+            //does this code on the frame it is called
+            yield return new WaitForSeconds(3); //waits three seconds
+                                                //then does the code down here
+
+            Rigidbody2D fireballPhysics = spawnedFireball.GetComponent<Rigidbody2D>();
+            Vector3 fireballForce = new Vector3(30, 7, 0);
+
+            fireballPhysics.AddForce(fireballForce, ForceMode2D.Impulse);
+
+            yield return new WaitForSeconds(3); //waits three more seconds
+                                                //then does the code down here
+
+            fireballForce = new Vector3(-30, 7, 0);
+
+            fireballPhysics.AddForce(fireballForce, ForceMode2D.Impulse);
+
+            //onTimer = false;
+            //never "truly" returns
+            //finishes here
         }
     }
 }
